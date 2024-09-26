6b4f9c40c12eda7163eb5810ce19934d06dea7a1
Mohammed-Manas <mohammadmanas2003@gmail.com>
2024-09-26 16:14:16 +0530

    Fully Uploaded

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import Gallery from './components/Gallery';
+import './App.css';
+
+function App() {
+
+  const galleryImages = [
+    {
+      img: "https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      img: "https://images.pexels.com/photos/3861458/pexels-photo-3861458.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      img: "https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      img: "https://images.pexels.com/photos/1194713/pexels-photo-1194713.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      img: "https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      img: "https://images.pexels.com/photos/3861458/pexels-photo-3861458.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+  ];
+
+  return (
+    <div className="App">
+      <br />
+      <div>
+        <strong>Responsive Photo Gallery in React JS</strong>
+      </div>
+      <br /><br />
+
+      <Gallery
+        galleryImages={galleryImages}
+      />
+
+      <br /><br />
+      <div>- MOHAMMED MANAS -</div>
+    </div>
+  );
+}
+
+export default App;
